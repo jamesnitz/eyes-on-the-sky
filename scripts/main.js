dec77f78c412650cfbf42cf4408e90d1dc7babbb
jamesnitz <jamesanitz2@gmail.com>
2019-12-31 13:42:09 -0600

    running json and can now favorite photos

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,12 @@ import potdListComponent from "./potd/potdListComponent.js";
 import marsPhotoListComponent from "./photos/marsPhotoListComponent.js";
 import { searchButtonListener } from "./photos/searchButtonMars.js";
 import { potdSearch } from "./potd/potdSearch.js";
+import { favoritesComponent } from "./favorites/favoritesComponent.js";
+import { favoritesList } from "./favorites/favoritesList.js";
 
-marsPhotoListComponent()
-potdListComponent()
-potdSearch()
+marsPhotoListComponent();
+potdListComponent();
+potdSearch();
 searchButtonListener();
+favoritesComponent();
+favoritesList()
